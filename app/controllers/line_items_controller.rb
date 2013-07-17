46364519c1834ac4f159cc5e49b1ec2b939a8d5a
nilinlin <nllnini@163.com>
2013-07-17 15:26:03 +0800

    depot ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class LineItemsController < ApplicationController
   # GET /line_items
   # GET /line_items.json
@@ -43,12 +51,12 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-  
+    @line_item.product = product
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart}
-          
+        format.html { redirect_to store_url }
+        format.js   { @current_item = @line_item }
         format.json { render json: @line_item,
           status: :created, location: @line_item }
       else
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,10 +1,10 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
-  attr_accessible :cart_id, :product_id, :quantity
+  attr_accessible :cart_id, :product_id
   attr_accessible :product
-  
+
   def total_price
-    product.price*quantity
+    product.price * quantity
   end
 end
