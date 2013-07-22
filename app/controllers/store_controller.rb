fba8e7afddaff6a4d8279629b6d12233142564ea
lc <631979719@qq.com>
2013-07-22 09:46:28 +0800

    add search function in stores

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,5 +8,26 @@ class StoreController < ApplicationController
 
     @products = Product.order(:title)
     @cart = current_cart
+
+    if params[:search] and params[:search].lstrip != ""
+      #product = Product.where("title = ?", params[:search])
+
+      product = []
+      products = Product.all
+      products.each  do |pro|
+        if pro.title.upcase.include?(params[:search].upcase)
+          product.push(pro)
+        end
+      end
+      
+      if product.length != 0
+        @products = product
+      else
+         respond_to do |format|
+           format.html { redirect_to store_url, notice: "#{params[:search]} not found" }
+         end
+      end
+    end
+        
   end
 end
