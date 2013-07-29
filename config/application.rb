b749a39e8cdd8d7c6cfed297bdaae5acb2983ce7
lc <631979719@qq.com>
2013-07-29 10:25:37 +0800

    redeploy in heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,20 @@ module Depot
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "myapp.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+    }    
   end
 end
