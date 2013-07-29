bf72863421e7e03f786390f65e6bf5d0170127c2
lc <631979719@qq.com>
2013-07-29 17:21:33 +0800

    merge

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,8 @@ module Depot
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
+    config.assets.initialize_on_precompile=false
+    
     config.assets.version = '1.0'
     
     config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
