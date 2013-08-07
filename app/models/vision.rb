9f57b9bd53770d8352f503ce76f0dd8815d7ae20
Fat-z <2641580591@qq.com>
2013-08-07 08:55:03 +0800

    Update vision.rb

--- app/models/vision.rb
+++ app/models/vision.rb
@@ -1,3 +1,5 @@
 class Vision < ActiveRecord::Base
   attr_accessible :publisher, :taker, :title, :number, :comment, :email
+  validates :title, :comment, :number, :email, presence: true
+  validates :number, numericality: {greater_than_or_equal_to: 1}
 end
